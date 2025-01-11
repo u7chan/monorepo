a6806bad44592ffd398e39e7e264d7feb13a31f5
u7chan <34462401+u7chan@users.noreply.github.com>
2025-01-11 23:15:54 +0900

    refactor: apply format

--- projects/hono-react/src/client.tsx
+++ projects/hono-react/src/client.tsx
@@ -1,14 +1,14 @@
-import type { FC } from "react";
-import { createRoot } from "react-dom/client";
+import type { FC } from 'react'
+import { createRoot } from 'react-dom/client'
 
-import { Main } from "./components/Main";
+import { Main } from './components/Main'
 
 const App: FC = () => {
-	return <Main />;
-};
+  return <Main />
+}
 
-const dom = document.getElementById("root");
+const dom = document.getElementById('root')
 if (dom) {
-	const root = createRoot(dom);
-	root.render(<App />);
+  const root = createRoot(dom)
+  root.render(<App />)
 }
--- projects/hono-react/src/components/Main.tsx
+++ projects/hono-react/src/components/Main.tsx
@@ -1,50 +1,50 @@
-import type { FC, FormEvent } from "react";
-import { hc } from "hono/client";
+import type { FC, FormEvent } from 'react'
+import { hc } from 'hono/client'
 
-import type { AppType } from "../server";
+import type { AppType } from '../server'
 
-const client = hc<AppType>("/");
+const client = hc<AppType>('/')
 
 export const Main: FC = () => {
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const formData = new FormData(e.currentTarget);
-		try {
-			const res = await client.api.profile.$post({
-				form: {
-					name: `${formData.get("name")}`,
-					email: `${formData.get("email")}`,
-				},
-			});
-			if (!res.ok) {
-				const { error } = (await res.json()) as unknown as {
-					error: {
-						issues: { message: string; path: string[] }[];
-					};
-				};
-				throw new Error(
-					`Validation Error: ${error.issues
-						.map(({ message, path }) => `'${path[0]}': ${message}`)
-						.join(", ")}`,
-				);
-			}
-		} catch (e: unknown) {
-			alert(e instanceof Error && e.message);
-		}
-	};
-	return (
-		<>
-			<h1>Profile</h1>
-			<hr />
-			<form onSubmit={handleSubmit}>
-				<div>
-					<input type="text" name="name" placeholder="name" required />
-				</div>
-				<div>
-					<input type="email" name="email" placeholder="email" required />
-				</div>
-				<button type="submit">Save</button>
-			</form>
-		</>
-	);
-};
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    try {
+      const res = await client.api.profile.$post({
+        form: {
+          name: `${formData.get('name')}`,
+          email: `${formData.get('email')}`,
+        },
+      })
+      if (!res.ok) {
+        const { error } = (await res.json()) as unknown as {
+          error: {
+            issues: { message: string; path: string[] }[]
+          }
+        }
+        throw new Error(
+          `Validation Error: ${error.issues
+            .map(({ message, path }) => `'${path[0]}': ${message}`)
+            .join(', ')}`,
+        )
+      }
+    } catch (e: unknown) {
+      alert(e instanceof Error && e.message)
+    }
+  }
+  return (
+    <>
+      <h1>Profile</h1>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input type='text' name='name' placeholder='name' required />
+        </div>
+        <div>
+          <input type='email' name='email' placeholder='email' required />
+        </div>
+        <button type='submit'>Save</button>
+      </form>
+    </>
+  )
+}
