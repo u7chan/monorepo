86c1f5d41a49a00c0974cbd9c39b7a7bc74b6476
u7chan <34462401+u7chan@users.noreply.github.com>
2025-02-08 21:08:23 +0900

    feat: example about markdown

--- projects/hono-react/src/routes/about.lazy.tsx
+++ projects/hono-react/src/routes/about.lazy.tsx
@@ -1,9 +1,10 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { About } from '../components/About'
 
 export const Route = createLazyFileRoute('/about')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <h2 className='font-semibold text-xl'>About</h2>
+  return <About />
 }
