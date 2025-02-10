879ac112e9c38924ea887845e608f97c63d9dff7
u7chan <34462401+u7chan@users.noreply.github.com>
2025-02-10 20:19:44 +0900

    refactor: HamburgerIcon

--- projects/hono-react/src/components/svg/HamburgerIcon.tsx
+++ projects/hono-react/src/components/svg/HamburgerIcon.tsx
@@ -1,22 +1,22 @@
 interface Props {
-  width?: number | string
-  height?: number | string
+  size?: number
+  color?: string
 }
 
-export function HamburgerIcon({ width, height }: Props) {
+export function HamburgerIcon({ size = 24, color = 'black' }: Props) {
   return (
     <svg
-      width={width || '24'}
-      height={height || '24'}
+      width={size}
+      height={size}
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
       <title>hamburger-icon</title>
       <rect width='24' height='24' fill='none' />
-      <rect x='3' y='6' width='18' height='2' fill='currentColor' />
-      <rect x='3' y='11' width='18' height='2' fill='currentColor' />
-      <rect x='3' y='16' width='18' height='2' fill='currentColor' />
+      <rect x='3' y='6' width='18' height='2' fill={color} />
+      <rect x='3' y='11' width='18' height='2' fill={color} />
+      <rect x='3' y='16' width='18' height='2' fill={color} />
     </svg>
   )
 }
