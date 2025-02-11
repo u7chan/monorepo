f98834d0129c22f12dbb9ce30e79428797ff2f86
u7chan <34462401+u7chan@users.noreply.github.com>
2025-02-12 00:50:47 +0900

    chore: test stream

--- projects/hono-react/src/llm/TestProvider.ts
+++ projects/hono-react/src/llm/TestProvider.ts
@@ -3,7 +3,10 @@ import type { LLMProvider, Messages, Reader } from './types'
 
 const TEST_DATA = `
 これはテスト用のストリームです🤖
+
 何回か繰り返しテキストを出力します。
+
+---
 
 # 見出し1
 ## 見出し2
@@ -132,7 +135,7 @@ function createDummyStream(messages: Messages[]): ReadableStreamDefaultReader<Ui
       await new Promise((resolve) => setTimeout(resolve, 25)) // delay
 
       if (chunkCount >= MAX_CHUNKS) {
-        const sloppyInputToken = messages.join('').length // 適当なトークン数
+        const sloppyInputToken = messages.map((x) => x.content).join('').length // 適当なトークン数
         controller.enqueue(
           new TextEncoder().encode(
             createResponsePayload({
