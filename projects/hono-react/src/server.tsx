608e3d4e7f4e73f783c3646dfdbaf61a15970fbf
u7chan <34462401+u7chan@users.noreply.github.com>
2025-02-13 23:54:56 +0900

    feat: favicon

--- projects/hono-react/src/server.tsx
+++ projects/hono-react/src/server.tsx
@@ -151,6 +151,7 @@ const app = new Hono<Env>()
           <head>
             <meta charSet='utf-8' />
             <meta content='width=device-width, initial-scale=1' name='viewport' />
+            <link rel='icon' href='/static/favicon.ico' />
             <link rel='stylesheet' href={prod ? '/static/main.css' : '/src/main.css'} />
             <script type='module' src={prod ? '/static/client.js' : '/src/client.tsx'} />
           </head>
