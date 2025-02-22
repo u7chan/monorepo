47ccca81265e34ad2dfc809e7d643c0162ed794e
u7chan <34462401+u7chan@users.noreply.github.com>
2025-02-22 20:45:04 +0900

    feat: setup fastapi

--- projects/llm-wrapper-api/main.py
+++ projects/llm-wrapper-api/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from llm-wrapper-api!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+async def root():
+    return {"status": "OK"}
