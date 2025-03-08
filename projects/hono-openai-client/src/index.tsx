1696895b6ebdb35212b91322b9d8a71a4991f2db
u7chan <34462401+u7chan@users.noreply.github.com>
2025-03-08 20:22:08 +0900

    feat: chatCompletions from openai

--- projects/hono-openai-client/src/index.tsx
+++ projects/hono-openai-client/src/index.tsx
@@ -1,12 +1,29 @@
 import { Hono } from 'hono'
 import { validator } from 'hono/validator'
+import OpenAI from 'openai'
 
 const app = new Hono()
+const openai = new OpenAI()
+
+async function chatCompletions(input: string): Promise<string> {
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'user',
+        content: input,
+      },
+    ],
+  })
+  const { content } = completion.choices[0].message
+  return content || ''
+}
 
 app.get(
   '/',
-  validator('query', (value, c) => {
-    const input = value['input']
+  validator('query', async (value, c) => {
+    const input = value['input'] as string | undefined
+    const message = input ? chatCompletions(input) : ''
     return c.html(
       <html lang='ja'>
         <head>
@@ -17,6 +34,7 @@ app.get(
             <input name='input' value={input} />
             <input type='submit' value='Send' />
           </form>
+          {message && <p>{message}</p>}
         </body>
       </html>
     )
