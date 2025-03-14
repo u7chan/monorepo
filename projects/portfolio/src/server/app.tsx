7f68c7d5d7102b2e4da3576e4b23f9214370a3f8
u7chan <34462401+u7chan@users.noreply.github.com>
2025-03-15 00:03:09 +0900

    feat: remove old llm implementation

--- projects/portfolio/src/server/app.tsx
+++ projects/portfolio/src/server/app.tsx
@@ -2,11 +2,10 @@ import { Hono } from 'hono'
 import { env } from 'hono/adapter'
 import { validator } from 'hono/validator'
 import { sValidator } from '@hono/standard-validator'
-import { streamSSE, streamText } from 'hono/streaming'
+import { streamSSE } from 'hono/streaming'
 import { renderToString } from 'react-dom/server'
 import { z } from 'zod'
 
-import { getLLMProvider } from './llm/getLLMProvider'
 import OpenAI from 'openai'
 import type { Stream } from 'openai/streaming'
 
@@ -118,111 +117,7 @@ const app = new Hono<Env>()
       }
     },
   )
-  .post(
-    '/api/chat_old',
-    sValidator(
-      'json',
-      z.object({
-        llm: z.enum(['openai', 'deepseek', 'test'], {
-          message: "llmは'openai','deepseek', 'test'のいずれかを指定してください",
-        }),
-        model: z.string().min(1),
-        temperature: z.number().min(0).max(1).nullish(),
-        maxTokens: z
-          .number()
-          .min(1, { message: 'maxTokensは1以上でなければなりません' })
-          .max(4096, { message: 'maxTokensは4096以下でなければなりません' })
-          .nullish(),
-        messages: z
-          .object({
-            role: z.enum(['user', 'assistant']),
-            content: z.string().min(1, { message: 'messagesは1文字以上でなければなりません' }),
-          })
-          .array(),
-      }),
-      (values, c) => {
-        const { success, error = [] } = values as {
-          success: boolean
-          error?: { message: string }[]
-        }
-        if (success) {
-          return
-        }
-        return c.json({ error: error.map((x) => x.message).join(',') || '' }, 400)
-      },
-    ),
-    async (c) => {
-      const { llm, model, temperature, maxTokens, messages } = c.req.valid('json')
-      const envs = env<{
-        OPENAI_API_KEY?: string
-        DEEPSEEK_API_KEY?: string
-      }>(c)
-      const llmProvider = getLLMProvider(llm, envs)
-      const reader = await llmProvider.chatStream(model, messages, temperature, maxTokens)
 
-      const decoder = new TextDecoder('utf-8')
-      let buffer = ''
-      return streamText(c, async (stream) => {
-        let aborted = false
-        stream.onAbort(() => {
-          aborted = true
-        })
-        while (true) {
-          const { done, value } = (await reader.read()) || {}
-          if (done || aborted) {
-            break
-          }
-
-          buffer += decoder.decode(value, { stream: true })
-          let boundary = buffer.indexOf('\n')
-
-          while (boundary !== -1) {
-            const chunk = buffer.slice(0, boundary).trim()
-            buffer = buffer.slice(boundary + 1)
-            boundary = buffer.indexOf('\n')
-
-            if (!chunk.startsWith('data:')) {
-              continue
-            }
-
-            const jsonStr = chunk.slice(5).trim() // 'data:'部分を除去
-            if (jsonStr !== '[DONE]') {
-              try {
-                const { choices, usage } = JSON.parse(jsonStr) as {
-                  id: string
-                  model: string
-                  choices: { delta: { content?: string }; finish_reason?: string }[]
-                  usage: {
-                    prompt_tokens: number
-                    completion_tokens: number
-                    total_tokens: number
-                  } | null
-                }
-                const text = choices.at(0)?.delta?.content || ''
-                const finishReason = choices.at(0)?.finish_reason || ''
-
-                if (finishReason) {
-                  console.log(`finish_reason=${finishReason}`)
-                }
-
-                if (usage) {
-                  console.log(
-                    `prompt_tokens=${usage.prompt_tokens}, completion_tokens=${usage.completion_tokens}`,
-                  )
-                }
-
-                await stream.writeln(
-                  `data: ${JSON.stringify({ content: text, finish_reason: finishReason, usage })}`,
-                )
-              } catch (error) {
-                console.error('Failed to parse JSON:', error)
-              }
-            }
-          }
-        }
-      })
-    },
-  )
   .get('*', (c) => {
     const { NODE_ENV } = env<{ NODE_ENV?: string }>(c)
     const prod = NODE_ENV === 'production'
