d6da30ed1a536b2e879ec54426d22e0da2c34aae
u7chan <34462401+u7chan@users.noreply.github.com>
2025-03-16 13:11:09 +0900

    feat: file input component

--- projects/portfolio/src/client/components/Profile.tsx
+++ projects/portfolio/src/client/components/Profile.tsx
@@ -1,7 +1,8 @@
-import type { FC, FormEvent } from 'react'
+import { useRef, useState, type FC, type FormEvent } from 'react'
 import { hc } from 'hono/client'
 
 import type { AppType } from '../../server/app'
+import { CloseIcon } from './svg/CloseIcon'
 
 const client = hc<AppType>('/')
 
@@ -52,6 +53,9 @@ export const Profile: FC = () => {
           className='w-full rounded-sm border border-gray-300 p-2'
         />
       </div>
+      <div className='mb-4'>
+        <FileInputContainer />
+      </div>
       <button
         type='submit'
         className='w-full rounded-sm bg-primary-800 p-2 text-white hover:bg-primary-700'
@@ -59,5 +63,75 @@ export const Profile: FC = () => {
         Save
       </button>
     </form>
+  )
+}
+
+function FileInputContainer() {
+  const [imagePreview, setImagePreview] = useState('')
+  const handleFileChange = (src: string) => {
+    setImagePreview(src)
+  }
+  const handleRemoveImage = () => {
+    setImagePreview('')
+  }
+  return (
+    <div>
+      {!imagePreview && <FileInput onFileChange={handleFileChange} />}
+      {imagePreview && <ImagePreview src={imagePreview} onRemoveImage={handleRemoveImage} />}
+    </div>
+  )
+}
+
+function FileInput({ onFileChange }: { onFileChange?: (src: string) => void }) {
+  const fileInputRef = useRef<HTMLInputElement | null>(null)
+  const handleClick = () => {
+    fileInputRef.current?.click()
+  }
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.[0]) {
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        onFileChange?.(reader.result as string)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
+  return (
+    <div>
+      <input
+        ref={fileInputRef}
+        type='file'
+        accept='image/*'
+        className='hidden'
+        onChange={handleFileChange}
+      />
+      <button
+        type='button'
+        onClick={handleClick}
+        className='mb-4 rounded bg-primary-800 px-4 py-2 text-white hover:bg-primary-700'
+      >
+        ファイルを選択
+      </button>
+    </div>
+  )
+}
+
+function ImagePreview({ src, onRemoveImage }: { src?: string; onRemoveImage?: () => void }) {
+  return (
+    <div className='relative'>
+      <img
+        src={src}
+        alt='Preview'
+        className='h-12 w-12 rounded-md border border-gray-300 object-cover'
+      />
+      <button
+        type='button'
+        onClick={onRemoveImage}
+        className='-top-[8px] absolute left-[38px] flex items-center justify-center rounded-full border bg-primary-800 hover:bg-primary-700'
+      >
+        <CloseIcon size={16} color='white' />
+      </button>
+    </div>
   )
 }
