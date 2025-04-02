22b782c2ed6d368ae5f724369b0a400b49e9e8fa
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-02 15:48:31 +0000

    feat: delete todo items

--- projects/honox-example-todo-list/app/routes/todos/index.tsx
+++ projects/honox-example-todo-list/app/routes/todos/index.tsx
@@ -2,16 +2,31 @@ import { createRoute } from 'honox/factory'
 import { css } from 'hono/css'
 
 export const POST = createRoute(async (c) => {
-  const { todo: title } = await c.req.parseBody<{ todo: string }>()
-  const newTodo = { id: Bun.randomUUIDv7(), title, content: '', done: false }
+  const { id, title } = await c.req.parseBody<{ id?: string; title?: string }>()
+
   const todos = (await Bun.file('app/data/todo.json').json()) as {
     id: string
     title: string
     content: string
     done: boolean
   }[]
-  const newTodos = [...todos, newTodo]
-  await Bun.write('app/data/todo.json', JSON.stringify(newTodos, null, 2))
+
+  if (title) {
+    // create
+    const newTodo = {
+      id: Bun.randomUUIDv7(),
+      title,
+      content: '',
+      done: false,
+    }
+    const newTodos = [...todos, newTodo]
+    await Bun.write('app/data/todo.json', JSON.stringify(newTodos, null, 2))
+  }
+  if (id) {
+    // delete
+    const newTodos = todos.filter((x) => x.id !== id)
+    await Bun.write('app/data/todo.json', JSON.stringify(newTodos, null, 2))
+  }
   return c.redirect('/todos')
 })
 
@@ -33,7 +48,7 @@ export default createRoute(async (c) => {
           gap: 8px;
         `}
       >
-        <input type="text" name="todo" placeholder="todo" required />
+        <input type="text" name="title" placeholder="todo" required />
         <input type="submit" value="Add" />
       </form>
       <ul>
@@ -52,6 +67,21 @@ export default createRoute(async (c) => {
             <input type="checkbox" id={todo.id} checked={todo.done} />
             <label for={todo.id}>{todo.title}</label>
             <a href={`/todos/${todo.id}`}>Detail</a>
+            <form method="post">
+              <input name="id" value={todo.id} hidden />
+              <button
+                type="submit"
+                class={css`
+                  background: red;
+                  color: white;
+                  border: none;
+                  padding: 4px;
+                  cursor: pointer;
+                `}
+              >
+                Delete
+              </button>
+            </form>
           </li>
         ))}
       </ul>
