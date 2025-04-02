5375faaeafacf0ffb65118f6b0a79085aceff90d
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-02 15:09:06 +0000

    feat: add detail page

---
+++ projects/honox-example-todo-list/app/routes/todos/[id].tsx
@@ -0,0 +1,35 @@
+import { createRoute } from 'honox/factory'
+import { css } from 'hono/css'
+
+export default createRoute(async (c) => {
+  const { id } = c.req.param<'/:id'>()
+  const { todos } = (await Bun.file('app/data/todo.json').json()) as {
+    todos: {
+      id: string
+      title: string
+      content: string
+      done: boolean
+    }[]
+  }
+
+  const todo = todos.find((x) => x.id === id)
+  if (!todo) {
+    // Not found items
+    return c.redirect('/todos')
+  }
+
+  return c.render(
+    <div>
+      <a href="/todos">Back</a>
+      <h1>{todo.title}</h1>
+      <div
+        class={css`
+          width: 90%;
+          height: 100px;
+        `}
+      >
+        {todo.content}
+      </div>
+    </div>
+  )
+})
