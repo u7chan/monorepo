e817ad0f60bf3589ccf90c8b7c0600f631e20ea2
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-02 15:39:51 +0000

    feat: new todo items

--- projects/honox-example-todo-list/app/routes/todos/index.tsx
+++ projects/honox-example-todo-list/app/routes/todos/index.tsx
@@ -1,18 +1,41 @@
 import { createRoute } from 'honox/factory'
 import { css } from 'hono/css'
 
+export const POST = createRoute(async (c) => {
+  const { todo: title } = await c.req.parseBody<{ todo: string }>()
+  const newTodo = { id: Bun.randomUUIDv7(), title, content: '', done: false }
+  const todos = (await Bun.file('app/data/todo.json').json()) as {
+    id: string
+    title: string
+    content: string
+    done: boolean
+  }[]
+  const newTodos = [...todos, newTodo]
+  await Bun.write('app/data/todo.json', JSON.stringify(newTodos, null, 2))
+  return c.redirect('/todos')
+})
+
 export default createRoute(async (c) => {
-  const { todos } = (await Bun.file('app/data/todo.json').json()) as {
-    todos: {
-      id: string
-      title: string
-      content: string
-      done: boolean
-    }[]
-  }
+  const todos = (await Bun.file('app/data/todo.json').json()) as {
+    id: string
+    title: string
+    content: string
+    done: boolean
+  }[]
   return c.render(
     <div>
       <h1>ToDo List</h1>
+      <form
+        method="post"
+        class={css`
+          margin-left: 40px;
+          display: flex;
+          gap: 8px;
+        `}
+      >
+        <input type="text" name="todo" placeholder="todo" required />
+        <input type="submit" value="Add" />
+      </form>
       <ul>
         {todos.map((todo) => (
           <li
