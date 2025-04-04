d6a708ebb5f8740934dc7c72196e646285b1c4c5
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-05 00:52:22 +0900

    feat: add build settings for production app

--- projects/chatbot-ui/src/dev-serve.ts
+++ projects/chatbot-ui/src/dev-serve.ts
@@ -1,11 +1,6 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+import app from './app'
 
 serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
