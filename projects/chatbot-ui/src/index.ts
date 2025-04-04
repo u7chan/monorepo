f77516a14c53d51dadc138f5f0be95d1d754fef1
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-04 15:27:42 +0000

    feat: setup biomejs

--- projects/chatbot-ui/src/index.ts
+++ projects/chatbot-ui/src/index.ts
@@ -7,9 +7,6 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
 })
