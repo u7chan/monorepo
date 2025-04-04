d38ce835506d9d35b60bcb45c6a35d2a1b3cb750
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-05 01:08:44 +0900

    chore: log

--- projects/chatbot-ui/src/dev-serve.ts
+++ projects/chatbot-ui/src/dev-serve.ts
@@ -3,5 +3,5 @@ import { serve } from '@hono/node-server'
 import app from './app'
 
 serve({ fetch: app.fetch, port: 3000 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+  console.log(`Dev Server is running on http://localhost:${info.port}`)
 })
