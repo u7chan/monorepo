6e0a8481107334f0ff4875a23b1191c2501042b9
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-06 12:06:35 +0900

    feat: add logger

--- projects/chatbot-ui/src/server/app.tsx
+++ projects/chatbot-ui/src/server/app.tsx
@@ -2,8 +2,10 @@ import type { Env } from '@/server/env'
 import { renderer } from '@/server/renderer'
 import { serveStatic } from '@hono/node-server/serve-static'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono<Env>()
+app.use(logger())
 app.use('/static/*', serveStatic({ root: './dist' }))
 app.use(renderer)
 
