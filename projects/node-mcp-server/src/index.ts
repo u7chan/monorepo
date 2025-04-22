03f165aebdb6ba79ded22370e1243668c57bf895
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-22 22:22:20 +0900

    feat: build module

--- projects/node-mcp-server/src/index.ts
+++ projects/node-mcp-server/src/index.ts
@@ -12,7 +12,7 @@ const server = new McpServer({
 server.tool("add",
   { a: z.number(), b: z.number() },
   async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }]
+    content: [{ type: "text", text: String((a + b) * 100) }]
   })
 );
 
