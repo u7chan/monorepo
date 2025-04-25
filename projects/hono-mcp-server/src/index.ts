1bd849f517b0d5222ba3ab815bd6a7885be8cd06
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-26 00:07:06 +0900

    feat: mcp server on hono

--- projects/hono-mcp-server/src/index.ts
+++ projects/hono-mcp-server/src/index.ts
@@ -1,9 +1,69 @@
 import { Hono } from 'hono'
+import { streamSSE } from 'hono/streaming'
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
+import { SSETransport } from 'hono-mcp-server-sse-transport'
+import { z } from 'zod'
+
+async function translateToEnglish(input: string): Promise<string> {
+  return `Translated to English: ${input}` // TODO: Implement actual translation logic
+}
+
+const mcpServer = new McpServer({
+  name: 'hono-mcp-server',
+  version: '1.0.0',
+})
+
+mcpServer.tool(
+  'Translate to English',
+  '入力された文章を英語に翻訳します',
+  { text: z.string() },
+  async (input) => {
+    return {
+      content: [
+        {
+          type: 'text',
+          text: await translateToEnglish(input.text),
+        },
+      ],
+    }
+  }
+)
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+// to support multiple simultaneous connections we have a lookup object from
+// sessionId to transport
+const transports: { [sessionId: string]: SSETransport } = {}
+
+app.get('/sse', (c) => {
+  return streamSSE(c, async (stream) => {
+    const transport = new SSETransport('/messages', stream)
+
+    transports[transport.sessionId] = transport
+
+    stream.onAbort(() => {
+      delete transports[transport.sessionId]
+    })
+
+    await mcpServer.connect(transport)
+
+    while (true) {
+      // This will keep the connection alive
+      // You can also await for a promise that never resolves
+      await stream.sleep(60_000)
+    }
+  })
+})
+
+app.post('/messages', async (c) => {
+  const sessionId = c.req.query('sessionId') || ''
+  const transport = transports[sessionId]
+
+  if (transport == null) {
+    return c.text('No transport found for sessionId', 400)
+  }
+
+  return transport.handlePostMessage(c)
 })
 
 export default app
