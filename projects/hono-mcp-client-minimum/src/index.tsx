2374539532739704d7e984cd98cbec4c3607c9cc
u7chan <34462401+u7chan@users.noreply.github.com>
2025-04-27 17:01:57 +0900

    feat: mcp client initialize

--- projects/hono-mcp-client-minimum/src/index.tsx
+++ projects/hono-mcp-client-minimum/src/index.tsx
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import { Client } from '@modelcontextprotocol/sdk/client/index.js'
+import { SSEClientTransport } from '@modelcontextprotocol/sdk/client/sse.js'
 import OpenAI from 'openai'
 
 export async function chat(model: string, query: string): Promise<string> {
@@ -23,6 +25,28 @@ export async function chat(model: string, query: string): Promise<string> {
   return response.choices[0].message.content || ''
 }
 
+async function getMCPClient() {
+  if (!process.env.MCP_SERVER_URL) {
+    throw new Error('MCP_SERVER_URL is not set')
+  }
+  console.log('Connecting to MCP server:', process.env.MCP_SERVER_URL)
+  const mcpClient = new Client({
+    name: 'hono-mcp-client',
+    version: '1.0.0',
+  })
+  const sseTransport = new SSEClientTransport(
+    new URL(process.env.MCP_SERVER_URL || '')
+  )
+  await mcpClient.connect(sseTransport)
+  const mcpTools = await mcpClient.listTools()
+  console.log(
+    'MCP Tools:',
+    mcpTools.tools.map(({ name, description }: any) => ({ name, description }))
+  )
+  return mcpClient
+}
+
+const mcpClient = await getMCPClient()
 const app = new Hono()
 
 app.post('/api/chat', async (c) => {
