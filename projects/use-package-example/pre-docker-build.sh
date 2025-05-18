0fd14d4cdcee6e5d301463a406eb19896dd631a9
u7chan <34462401+u7chan@users.noreply.github.com>
2025-05-18 14:02:32 +0900

    chore: log

--- projects/use-package-example/pre-docker-build.sh
+++ projects/use-package-example/pre-docker-build.sh
@@ -15,13 +15,23 @@ fi
 # 現在のスクリプトの絶対パスを取得
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
+# packagesディレクトリを作成
+mkdir -p "./packages"
+
 # 抽出したライブラリをループ処理
 echo "$WORKSPACE_LIBS" | sed -e 's/^\[//; s/\]$//; s/,//g; s/"//g; s/'"'"'//g' | while read -r WORKSPACE_PATH; do
     echo "Processing workspace library: $WORKSPACE_PATH"
+    path=$WORKSPACE_PATH
+    package_dir_name=$(basename "$path")
+    echo "Package directory name: $package_dir_name"
 
     # cpで現在のディレクトリにライブラリをコピー
-    mkdir -p "./packages"
     cp -r "$WORKSPACE_PATH" "./packages"
+
+    # package.jsonの置換処理
+    ## TODO: package.jsonのdependenciesを修正（以下のように置換したい）
+    ## before: "{package_dir_name}": "workspace:../../{package_dir_name}"
+    ## after: "{package_dir_name}": "workspace:{package_dir_name}"
 done
 
 echo "All workspace libraries processed successfully 🚀"
