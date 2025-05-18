5b1d174671a686f2113936290603dddaf9ff7a89
u7chan <34462401+u7chan@users.noreply.github.com>
2025-05-18 15:27:13 +0900

    feat: Add support for multiple monorepo libraries dependencies

---
+++ packages/example-lib-b/dist/index.js
@@ -0,0 +1,7 @@
+// src/index.ts
+function calculate(arg1, arg2) {
+  return arg1 + arg2;
+}
+export {
+  calculate
+};
--- projects/use-package-example/index.ts
+++ projects/use-package-example/index.ts
@@ -1,7 +1,9 @@
-import { getHelloMessage } from "example-lib";
+import { getHelloMessage } from "example-lib-a";
+import { calculate } from "example-lib-b";
 
 function main() {
   console.log(getHelloMessage());
+  console.log(calculate(5, 3));
 }
 
 main();
