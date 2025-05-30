8de71391a73a9a33cb3e89fc7697294284fb0e1f
u7chan <34462401+u7chan@users.noreply.github.com>
2025-05-30 21:06:11 +0900

    refactor: GitHub Actions to handle file renames and extract project logic

--- .github/actions/get-changed-directories/get-changed-dirs.sh
+++ .github/actions/get-changed-directories/get-changed-dirs.sh
@@ -8,8 +8,22 @@ set -e
 # 対象ディレクトリを定義
 TARGET_DIRS=("projects", "packages")
 
+# 関数: diff_with_renames
+# 引数:
+#   $1 - 比較したい最初のリファレンス（例: HEAD）
+#   $2 - 比較したい2番目のリファレンス（例: origin/main）
+# 機能:
+#   2つのリファレンス間の差分で、リネーム（移動）されたファイルの
+#   移動後のパスと、それ以外の変更ファイルを区別して出力します。
+diff_with_renames() {
+  # リネームされたファイルの移動後のパスを取得
+  git diff --diff-filter=R --name-status $1 $2 | awk '$1 ~ /^R/ {print $2}'
+  # その他の差分
+  git diff --name-only $1 $2 | grep -vFf <(git diff --name-only --diff-filter=R $1 $2)
+}
+
 echo "> diff"
-git diff --name-only HEAD origin/main
+diff_with_renames HEAD origin/main
 echo ""
 
 # 対象ディレクトリの配列をAWKに渡すための文字列を作成
@@ -18,7 +32,7 @@ TARGET_DIRS_STR=${TARGET_DIRS_STR%,}  # 末尾のカンマを削除
 
 # 最新のコミットととリモートのmainブランチの差分を取得
 # 対象ディレクトリ配下のファイルのディレクトリ名を取得
-git diff --name-only HEAD origin/main | \
+diff_with_renames HEAD origin/main | \
 awk -F/ -v target_dirs="$TARGET_DIRS_STR" '
   BEGIN {
     # 対象ディレクトリを動的に配列として定義
