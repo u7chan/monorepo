f1167804ad1b687dc67e587784e6f8b4d2d3cb36
u7chan <34462401+u7chan@users.noreply.github.com>
2025-06-07 20:36:01 +0900

    refactor: utils

--- packages/chatbot-ui/src/components/ui/textarea.tsx
+++ packages/chatbot-ui/src/components/ui/textarea.tsx
@@ -1,6 +1,6 @@
 import type * as React from 'react'
 
-import { cn } from '#/lib/utils'
+import { cn } from '#/components/ui/lib/utils'
 
 function Textarea({ className, ...props }: React.ComponentProps<'textarea'>) {
   return (
