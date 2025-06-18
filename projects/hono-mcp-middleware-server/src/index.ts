712bc22184a049254e93e62305cf9e3a5e37d871
u7chan <34462401+u7chan@users.noreply.github.com>
2025-06-18 23:45:33 +0900

    feat: reverse_string tools

--- projects/hono-mcp-middleware-server/src/index.ts
+++ projects/hono-mcp-middleware-server/src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 import { StreamableHTTPTransport } from '@hono/mcp'
 import { Hono } from 'hono'
+import z from 'zod'
 
 const app = new Hono()
 
@@ -8,6 +9,17 @@ const app = new Hono()
 const mcpServer = new McpServer({
   name: 'my-mcp-server',
   version: '1.0.0',
+})
+
+mcpServer.tool('reverse_string', '入力文字列を反転します', { text: z.string() }, ({ text }) => {
+  return {
+    content: [
+      {
+        type: 'text',
+        text: text.split('').reverse().join(''),
+      },
+    ],
+  }
 })
 
 app.all('/mcp', async (c) => {
