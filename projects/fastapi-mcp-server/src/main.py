3d03d82f139d03d4ca9f08d300fe7e1881dc9dcc
u7chan <34462401+u7chan@users.noreply.github.com>
2025-06-25 23:35:03 +0900

    feat: FastAPI-MCP example

--- projects/fastapi-mcp-server/src/main.py
+++ projects/fastapi-mcp-server/src/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi_mcp import FastApiMCP
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello Fast MCP World"}
+
+
+mcp = FastApiMCP(
+    app,
+    name="test",
+    description="test mcp server",
+)
+
+# Mount the MCP server directly to your FastAPI app
+mcp.mount()
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
