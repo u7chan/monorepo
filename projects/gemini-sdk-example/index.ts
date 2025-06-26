db85b04e29def67a41e3cf20b55fd0678fd7e357
u7chan <34462401+u7chan@users.noreply.github.com>
2025-06-26 20:05:17 +0900

    feat: Add streaming support and configure base URL for Gemini SDK

--- projects/gemini-sdk-example/index.ts
+++ projects/gemini-sdk-example/index.ts
@@ -1,13 +1,17 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY, httpOptions: {
+  baseUrl: "https://generativelanguage.googleapis.com"
+} });
 
 async function main() {
-  const response = await ai.models.generateContent({
+  const response = await ai.models.generateContentStream({
     model: "gemini-2.0-flash",
     contents: "なぜ空は青いの?",
   });
-  console.log(response.text);
+  for await (const chunk of response) {
+    console.log(chunk.text);
+  }
 }
 
 main();
