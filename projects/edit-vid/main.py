74b3daa9f31751c786dcc64aadb1080aedc35b9c
u7chan <34462401+u7chan@users.noreply.github.com>
2025-07-03 21:04:44 +0900

    feat: setup fastapi

--- projects/edit-vid/main.py
+++ projects/edit-vid/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from edit-vid!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
