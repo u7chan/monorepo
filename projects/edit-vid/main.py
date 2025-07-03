46de2d14ace6d646fe0d73cf0a81d1f5ad8489d2
u7chan <34462401+u7chan@users.noreply.github.com>
2025-07-03 21:12:40 +0900

    feat: Serve index.html at root endpoint

--- projects/edit-vid/main.py
+++ projects/edit-vid/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return FileResponse("index.html")
