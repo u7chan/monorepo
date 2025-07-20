451da490d20655bc670f25f65bf8f92a1bb49f6a
u7chan <34462401+u7chan@users.noreply.github.com>
2025-07-20 11:18:09 +0900

    chore: update configuration files and enhance file listing in the server

--- projects/file_server/src/index.ts
+++ projects/file_server/src/index.ts
@@ -1,9 +1,19 @@
+import { readdir } from "node:fs/promises";
 import { Hono } from "hono";
+import { env } from "hono/adapter";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    UPLOAD_DIR: string;
+  };
+}>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const uploadDir = env(c).UPLOAD_DIR || "./tmp";
+  const files = await readdir(uploadDir);
+  return c.json({
+    files,
+  });
 });
 
 export default app;
