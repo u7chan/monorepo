97f2e0b2ad573c034f7bba25b4c0f064d835663d
u7chan <34462401+u7chan@users.noreply.github.com>
2025-08-06 23:08:35 +0900

    feat: llm pages

--- projects/portfolio/tailwind.config.ts
+++ projects/portfolio/tailwind.config.ts
@@ -2,6 +2,7 @@ import typography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
