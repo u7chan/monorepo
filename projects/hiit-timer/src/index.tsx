fe5164c8646aa2f6c22316a3bf8c110dadede21d
u7chan <34462401+u7chan@users.noreply.github.com>
2026-01-11 11:11:26 +0900

    feat: update root route to return a basic Tailwind CSS HTML structure

--- projects/hiit-timer/src/index.tsx
+++ projects/hiit-timer/src/index.tsx
@@ -3,7 +3,18 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.html(<>Hello</>)
+  return c.html(
+    <html>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      </head>
+      <body>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      </body>
+    </html>
+  )
 })
 
 export default app
