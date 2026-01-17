5b6bca200dcb7b724712fdb16e54951986a9f74b
u7chan <34462401+u7chan@users.noreply.github.com>
2026-01-17 10:51:21 +0900

    chore: clean

--- projects/u7chat/src/features/chat/chat-message.tsx
+++ projects/u7chat/src/features/chat/chat-message.tsx
@@ -15,7 +15,7 @@ interface ChatMessageProps {
 export function ChatMessage({ messages, streamMessage, scrollContainer }: ChatMessageProps) {
   return (
     <div className='flex flex-col gap-2'>
-      <div className='flex  flex-col gap-2 overflow-y-auto p-4'>
+      <div className='flex flex-col gap-2 overflow-y-auto p-4'>
         {messages.map((message,i ) => (
           <React.Fragment key={i}>
             {message.role === 'user' && (
