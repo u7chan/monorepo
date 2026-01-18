557fcfccf1c5060bf5126f3ad56c05e52fdaf6d1
u7chan <34462401+u7chan@users.noreply.github.com>
2026-01-18 14:55:23 +0900

    feat: add DiffViewer component with split-view diff highlighting

--- projects/portfolio/src/client/pages/diff/index.tsx
+++ projects/portfolio/src/client/pages/diff/index.tsx
@@ -1,3 +1,17 @@
+import ReactDiffViewer from 'react-diff-viewer'
+
 export function Diff() {
-  return   <div className='h-screen overflow-y-auto bg-white p-4 dark:bg-gray-900'>Diff Viewer</div>
+  const oldCode = `line 1
+line 2
+line 3`
+
+  const newCode = `line 1
+modified line 2
+line 3`
+
+  return (
+    <div className='h-screen overflow-y-auto bg-white p-4 dark:bg-gray-900'>
+      <ReactDiffViewer oldValue={oldCode} newValue={newCode} splitView={true} />
+    </div>
+  )
 }
