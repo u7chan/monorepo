af7691c9ba121feda00fa3cc75dacbee020c02a6
u7chan <34462401+u7chan@users.noreply.github.com>
2026-02-07 15:51:29 +0900

    chore: stroke-width -> strokeWidth

--- projects/portfolio/src/client/pages/diff/index.tsx
+++ projects/portfolio/src/client/pages/diff/index.tsx
@@ -145,13 +145,13 @@ export function Diff() {
         >
           {mode === 'view' ? (
             <svg className='w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
-              <path stroke-width='2.5' d='M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 2-2v-7'></path>
-              <path stroke-width='2.5' d='m18.5 2.5 3 3L12 15l-4 1 1-4 9.5-9.5z'></path>
+              <path strokeWidth='2.5' d='M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 2-2v-7'></path>
+              <path strokeWidth='2.5' d='m18.5 2.5 3 3L12 15l-4 1 1-4 9.5-9.5z'></path>
             </svg>
           ) : (
             <svg className='w-4 h-4' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
-              <path stroke-width='2.5' d='M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z'></path>
-              <circle stroke-width='2.5' cx='12' cy='12' r='3'></circle>
+              <path strokeWidth='2.5' d='M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z'></path>
+              <circle strokeWidth='2.5' cx='12' cy='12' r='3'></circle>
             </svg>
           )}
         </button>
