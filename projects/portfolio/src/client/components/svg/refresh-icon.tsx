3ed46ee4cae2a97cfa662dc80bea5ddfd930c7ff
u7chan <34462401+u7chan@users.noreply.github.com>
2026-05-23 17:19:42 +0900

    fix(portfolio): update refresh icon design (#946)
    * fix(portfolio): update refresh icon design
    
    Replace the previous refresh icon with a cleaner SVG design embedded from Jam icons.
    
    * style: apply formatting to refresh-icon.tsx

--- projects/portfolio/src/client/components/svg/refresh-icon.tsx
+++ projects/portfolio/src/client/components/svg/refresh-icon.tsx
@@ -2,11 +2,11 @@ import { type IconProps, SvgIcon } from './icon-base'
 
 export function RefreshIcon({ size = 24, className = 'fill-black dark:fill-white' }: IconProps) {
   return (
-    <SvgIcon size={size} title='refresh-icon'>
+    <SvgIcon size={size} viewBox='-1.5 -2.5 24 24' title='refresh-icon'>
       <path
         fillRule='evenodd'
         clipRule='evenodd'
-        d='M4 12a8 8 0 0 1 12.93-6.146l-1.065 1.065A6 6 0 1 0 18 12h-3l3.707 3.707a1 1 0 0 0 1.414 0L23.83 12H19a10 10 0 1 1-15-7.071V1a1 1 0 0 1 2 0v4a1 1 0 0 1-1 1H1a1 1 0 0 1 0-2h3.343A8 8 0 0 1 4 12z'
+        d='M17.83 4.194l.42-1.377a1 1 0 1 1 1.913.585l-1.17 3.825a1 1 0 0 1-1.248.664l-3.825-1.17a1 1 0 1 1 .585-1.912l1.672.511A7.381 7.381 0 0 0 3.185 6.584l-.26.633a1 1 0 1 1-1.85-.758l.26-.633A9.381 9.381 0 0 1 17.83 4.194zM2.308 14.807l-.327 1.311a1 1 0 1 1-1.94-.484l.967-3.88a1 1 0 0 1 1.265-.716l3.828.954a1 1 0 0 1-.484 1.941l-1.786-.445a7.384 7.384 0 0 0 13.216-1.792 1 1 0 1 1 1.906.608 9.381 9.381 0 0 1-5.38 5.831 9.386 9.386 0 0 1-11.265-3.328z'
         className={className}
       />
     </SvgIcon>
